4a92227ddc8f831a32837ce36039f776d3be8a21
Anjan Nair <anjannair56@gmail.com>
2022-04-16 13:03:18 +0530

    External IP instead of local

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import socket
 import requests
+import urllib.request
 
 webhookURL = '<webhook url>'
 hostname = socket.gethostname()
-localIP = socket.gethostbyname(hostname)
+external_ipv6 = urllib.request.urlopen('https://v6.ident.me').read().decode('utf8')
+external_ipv4 = urllib.request.urlopen('https://v4.ident.me').read().decode('utf8')
+
 
 data = {
     'username': 'Server IP Webhook',
     'embeds': [{
         'title': 'Server started!',
-        'description': hostname + ': ' + localIP,
+        'description': hostname + '\n' + 'IPV6' + ': ' + '`' + external_ipv6 + '`'  + '\n\n' + 'IPV4 ' + ':'  + '`' + external_ipv4 + '`',
     }]
 }
 
